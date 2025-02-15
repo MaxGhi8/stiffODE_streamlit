4b62b021ef9e7d72cd0bf7a630a773605ff21014
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-02-15 14:41:19 +0100

    do the same for the HH problem

--- pages/2_Hodgkin-Huxkley.py
+++ pages/2_Hodgkin-Huxkley.py
@@ -1,10 +1,11 @@
-import matplotlib.pyplot as plt
 import streamlit as st
+import sys
 
-st.set_page_config(layout="wide")
+sys.path.append("..")
+from utilities import plot_input, plot_outputs, plot_errors
 
 
-def fhn_page():
+def hh_page():
     st.title("Hodgkin-Huxley model")
 
     st.markdown(
@@ -14,50 +15,103 @@ def fhn_page():
         describe the dynamics of the membrane potential and the gating variables of ion channels.
     """
     )
+    ## Selection of the model
+    str_model = st.sidebar.selectbox(
+        "Select the model",
+        ["best", "best_samedofs"],
+    )
 
-    st.header("Input Function")
+    ## Selection of the indexes
+    sample_idxs = []
+    initial_values = [250, 280, 310, 340]
+    for idx, initial_value in enumerate(initial_values):
+        sample_idx = st.sidebar.number_input(
+            f"Index of the {idx + 1} column",
+            min_value=0,
+            max_value=374,
+            value=initial_value,
+            step=1,
+            key=f"sample_idx_{idx}",
+        )
+        sample_idxs.append(sample_idx)
 
-    # Create columns for visualization
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        plot_function()
+    ## Plot of the input
+    # st.header("Input Function")
+    cols = st.columns(4)
+    for idx, (col, sample_idx) in enumerate(zip(cols, sample_idxs)):
+        with col:
+            if idx == 0:
+                st.plotly_chart(
+                    plot_input("HH", str_model, "input", sample_idx, "Current (mA)"),
+                    key=f"input_{sample_idx}",
+                )
+            else:
+                st.plotly_chart(
+                    plot_input("HH", str_model, "input", sample_idx),
+                    key=f"input_{sample_idx}",
+                )
 
-    with col2:
-        plot_true_solution()
+    ## Plot of the output and the errors
+    # Store the number of sets in session state
+    if "num_variables_hh" not in st.session_state:
+        st.session_state.num_variables_hh = 1
 
-    with col3:
-        plot_approximate_solution()
+    # Display all the variables
+    for var in range(st.session_state.num_variables_hh):
 
-    with col4:
-        plot_error()
+        # Selection of the variable
+        cols = st.columns(4)
 
+        with cols[0]:
+            str_variable = st.selectbox(
+                "Select the variable to plot",
+                ["V", "m", "h", "n"],
+                index=var,
+                key=f"variable_{var}",  # Unique key for each selectbox
+            )
 
-def placeholder_plot():
-    # Placeholder function for actual plots
-    fig = plt.figure()
-    plt.plot([0, 1], [0, 1])
-    return fig
+        # outputs
+        cols = st.columns(4)
+        for idx, (col, sample_idx) in enumerate(zip(cols, sample_idxs)):
+            with col:
+                if idx == 0:
+                    st.plotly_chart(
+                        plot_outputs(
+                            "HH", str_model, str_variable, sample_idx, "Voltage (mV)"
+                        ),
+                        key=f"output_{sample_idx}_var_{var}",
+                    )
+                else:
+                    st.plotly_chart(
+                        plot_outputs("HH", str_model, str_variable, sample_idx),
+                        key=f"output_{sample_idx}_var_{var}",
+                    )
+        # errors
+        cols = st.columns(4)
+        for idx, (col, sample_idx) in enumerate(zip(cols, sample_idxs)):
+            with col:
+                if idx == 0:
+                    st.plotly_chart(
+                        plot_errors(
+                            "HH",
+                            str_model,
+                            str_variable,
+                            sample_idx,
+                            "Module of the error",
+                        ),
+                        key=f"error_{sample_idx}_var_{var}",
+                    )
+                else:
+                    st.plotly_chart(
+                        plot_errors("HH", str_model, str_variable, sample_idx),
+                        key=f"error_{sample_idx}_var_{var}",
+                    )
 
-
-def plot_function():
-    # Plot input function (replace with actual function)
-    st.pyplot(placeholder_plot())
-
-
-def plot_true_solution():
-    # Plot true solution (replace with actual solution)
-    st.pyplot(placeholder_plot())
-
-
-def plot_approximate_solution():
-    # Plot approximate solution (replace with actual approximation)
-    st.pyplot(placeholder_plot())
-
-
-def plot_error():
-    # Plot error (replace with actual error)
-    st.pyplot(placeholder_plot())
+    # Button to add more sets
+    if st.button("Add another variable to plot"):
+        st.session_state.num_variables_hh += 1
 
 
 if __name__ == "__main__":
-    fhn_page()
+    st.set_page_config(layout="wide")
+    hh_page()
